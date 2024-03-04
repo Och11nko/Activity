e9a7b5a99e8c767f3fc71c6e73b855018642e152
Och11nko <157588989+Och11nko@users.noreply.github.com>
2024-03-04 22:19:22 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+        numb: 3,
+        question: "What is the purpose of the <div> tag in HTML?",
+        answer: "To group together HTML elements for styling or manipulation",
+        options: [
+          "To group together HTML elements for styling or manipulation",
+          "To define hyperlink",
+          "To create an ordered list",
+          "To embed an image"
+    ]
+  },
+    {
+        numb: 4,
+        question: "What is the difference between padding and margin in CSS?",
+        answer: "Padding adds space inside an element, while margin adds space outside the element",
+        options: [
+          "Margin adds space inside an element, while padding adds space outside the element",
+          "Padding and margin are the same thing",
+          "Padding and margin are both control the width of an element",
+          "Padding adds space inside an element, while margin adds space outside the element"
+    ]
+  },
+    {
+        numb: 5,
+        question: "What does the typeof operator do in JavaScript?",
+        answer: "It returns the data type of a variable or expression as a string",
+        options: [
+          "It returns the data type of a variable or expression as a string",
+          "It converts a variable to a different data type",
+          "It checks if a variable is undefined",
+          "It determines the length of a string"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
